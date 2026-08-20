0d75cc2638f24d8e9a2d8087facf6faf7f6c2e54
mehdichamam <mehdichamam465@gmail.com>
2026-08-20 15:13:01 +0000

    Checking Data Quality For orders Table

--- sql/04_data_quality.sql
+++ sql/04_data_quality.sql
@@ -1,7 +1,53 @@
-SELECT order_id,
-       SUM(order_id IS NULL),
-       SUM(customer_id IS NULL),
-       SUM(order_status IS NULL)
+-- Checking For Missing Values
+SELECT 
+       SUM(order_id IS NULL) as Missing_id,
+       SUM(customer_id IS NULL) as Missing_customer_id,
+       SUM(order_status IS NULL) as Missing_status
 FROM orders;
 
-SELECT * FROM orders;
+-- Checking For Duplicates
+
+SELECT order_id , count(*) as N
+FROM orders
+GROUP BY order_id
+HAVING N > 1;
+
+-- Checking for invalid Relationships
+
+SELECT count(*) as "Orphan Keys"
+FROM orders o
+LEFT JOIN customers c
+ON o.customer_id = c.customer_id
+WHERE c.customer_id IS NULL;
+
+-- Checking For Invalid Dates
+SELECT *
+FROM orders
+WHERE 
+(
+    order_purchase_timestamp IS NOT NULL
+    And order_approved_at IS NOT NULL
+    And order_purchase_timestamp > order_approved_at
+)
+OR
+(
+    order_approved_at IS NOT NULL
+    And order_delivered_carrier_date IS NOT NULL
+    AND order_approved_at > order_delivered_carrier_date
+)
+OR
+(
+    order_delivered_carrier_date IS NOT NULL
+    AND order_delivered_customer_date IS NOT NULL
+    AND order_delivered_carrier_date > order_delivered_customer_date
+);
+
+SELECT
+    order_status,
+    COUNT(*) AS orders,
+    SUM(order_approved_at IS NULL) AS missing_approved,
+    SUM(order_delivered_carrier_date IS NULL) AS missing_carrier,
+    SUM(order_delivered_customer_date IS NULL) AS missing_customer
+FROM orders
+GROUP BY order_status;
+
